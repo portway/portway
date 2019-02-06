5895af93107c26628888076b6f0672967c8c5cde
JJ Idt <johnidt@gmail.com>
2019-02-05 16:23:35 -0800

    cleanup

--- api/coordinators/billing.js
+++ api/coordinators/billing.js
@@ -1,11 +1,11 @@
 import stripeIntegrator from '../integrators/stripe'
 
 const subscribeCustomerToPlan = async function({ token, planId, email }) {
-  console.log(token, planId, email)
   // 1 create customer in stripe - email and source ie token
   const customer = await stripeIntegrator.createCustomer({ email, token })
 
-  // 2 TODO: save customer info to db - email and customer id, token too?
+  // 2 TODO: save customer info to db - email and customer id, token too?, depending on how sign up flow goes, user might already have account,
+  // in that case look up user and update
 
   // 3 subscribe customer to plan - customer id and plan id
   await stripeIntegrator.createSubscription({ customerId: customer.id, planId })
