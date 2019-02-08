3ab190981a3b8872f9109ba6be2ecdd221468653
JJ Idt <johnidt@gmail.com>
2019-02-07 17:04:32 -0800

    add unit tests for billing coordinator

---
+++ api/src/coordinators/__mocks__/billing.js
@@ -0,0 +1,3 @@
+export default {
+  subscribeCustomerToPlan: jest.fn()
+}
--- api/src/coordinators/billing.test-u.js
+++ api/src/coordinators/billing.test-u.js
@@ -1,14 +1,17 @@
 import billingCoordinator from './billing'
 import stripeIntegrator from '../integrators/stripe'
 
+jest.mock('../integrators/stripe')
+
 describe('billing coordinator', () => {
   describe('#subscribeCustomerToPlan', () => {
     const token = 'not-a-real-token'
     const planId = 'not-a-real-plan-id'
     const email = 'test@bonkeybong.com'
+    let resolvedValue
 
     beforeAll(async () => {
-      await billingCoordinator.subscribeCustomerToPlan({
+      resolvedValue = await billingCoordinator.subscribeCustomerToPlan({
         token,
         planId,
         email
@@ -16,8 +19,23 @@ describe('billing coordinator', () => {
     })
 
     it('should call stripeIntegrator.createCustomer with the customer email and card token', () => {
-      expect(stripeIntegrator.createCustomer).toHaveBeenCalledTimes(1)
-      expect(stripeIntegrator.createCustomer).toHaveBeenCallWith({ email, token })
+      expect(stripeIntegrator.createCustomer.mock.calls.length).toBe(1)
+      expect(stripeIntegrator.createCustomer.mock.calls[0][0]).toEqual({
+        email,
+        token
+      })
+    })
+
+    it('should call stripeIntegrator.createSubscription with the customer id and plan id', () => {
+      expect(stripeIntegrator.createSubscription.mock.calls.length).toBe(1)
+      expect(stripeIntegrator.createSubscription.mock.calls[0][0]).toEqual({
+        customerId: stripeIntegrator.createCustomer.mock.results[0].value.id,
+        planId
+      })
+    })
+
+    it('should resolve with undefined', () => {
+      expect(resolvedValue).toBe(undefined)
     })
   })
 })
