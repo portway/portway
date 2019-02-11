8f3d7b61c582367760cd2d4131e0a07b2e3e2f6d
Jay Contonio <jay@bonkeybong.com>
2019-02-11 15:10:36 -0800

    A working emphasis

--- web/src/client/components/EditorComponent/EditorComponent.js
+++ web/src/client/components/EditorComponent/EditorComponent.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { debounce } from '../../../shared/utilities'
+import { throttle } from '../../../shared/utilities'
+
+// https://gist.github.com/jbroadway/2836900
 
 import './EditorComponent.scss'
 
@@ -8,84 +10,106 @@ class EditorComponent extends React.Component {
     super(props)
     this.editorRef = React.createRef()
     this.state = {
-      inputs: []
+      tree: []
     }
   }
 
   componentDidMount() {
-    const inputs = this.editorRef.current.querySelectorAll(
-      '.editor-input'
-    )
-    this.setState({ inputs: inputs })
-    inputs[inputs.length - 1].focus()
-    document.execCommand('insertHTML', false, '')
-    document.addEventListener(
-      'keydown',
-      this.keydownHandler.bind(this),
-      false
-    )
-    document.addEventListener(
-      'keyup',
-      debounce(200, this.keyupHandler.bind(this)),
-      false
-    )
+    // Get a div in there for the first item
+    const nd = document.createElement('div')
+    nd.appendChild(document.createElement('br'))
+    this.editorRef.current.appendChild(nd)
+    nd.focus()
+    // Set up content editor
+    document.execCommand('insertHTML', false, 'div')
+    // document.execCommand('styleWithCSS', true)
+    document.addEventListener('keydown', this.keydownHandler.bind(this), false)
+    document.addEventListener('keyup', throttle(this.keyupHandler.bind(this)), false)
   }
 
   render() {
     return (
-      <div className="editor" ref={this.editorRef}>
-        <div className="editor-input" contentEditable />
+      <div className="editor">
+        <div className="editor-input" contentEditable ref={this.editorRef} />
       </div>
     )
   }
 
   keyupHandler(e) {
-    const currentInput = e.target
-    if (
-      currentInput.textContent &&
-      currentInput.textContent.match(/^#\s/)
-    ) {
-      currentInput.classList.add('h1')
-    } else {
-      currentInput.classList.remove('h1')
+    const el = window.getSelection()
+
+    if (el.focusNode) {
+      // Don't process already processed elements
+      if (el.focusNode.parentElement.tagName === 'EM') {
+        return
+      }
+
+      // Process the node's textContent
+      if (el.focusNode.nodeType === 3 && el.focusNode.textContent) {
+        let patternMatch
+        const emphasisPattern = /(\*|_)(.*?)\1/g
+        while ((patternMatch = emphasisPattern.exec(el.focusNode.textContent)) != null) {
+          if (el.focusNode.parentElement.tagName !== 'EM') {
+            // Create the range to select
+            const range = document.createRange()
+            range.setStart(el.focusNode, patternMatch.index)
+            range.setEnd(el.focusNode, emphasisPattern.lastIndex)
+            // Create the new selection
+            el.removeAllRanges()
+            el.addRange(range)
+            // Format that selection
+            document.execCommand('insertHTML', false, `<em>${el.toString()}</em>&nbsp;`)
+            el.collapseToEnd()
+          }
+        }
+      }
+    }
+
+    // const range = document.createRange()
+    // range.setStart(el.focusNode, patternMatch.index)
+    // range.setEnd(el.focusNode, emphasisPattern.lastIndex)
+    // // Make the selection and italicize it
+    // el.addRange(range)
+    // document.execCommand('italic', false)
+    // el.collapseToEnd()
+
+    // If the element should be a certain one but isn't
+    if (el.anchorNode && el.anchorNode.nodeType === 3) {
+      if (/^#\s/.test(el.anchorNode.textContent)) {
+        document.execCommand('formatBlock', false, 'h1')
+      }
+      if (/^##\s/.test(el.anchorNode.textContent)) {
+        document.execCommand('formatBlock', false, 'h2')
+      }
+      if (/^###\s/.test(el.anchorNode.textContent)) {
+        document.execCommand('formatBlock', false, 'h3')
+      }
+      if (/^####\s/.test(el.anchorNode.textContent)) {
+        document.execCommand('formatBlock', false, 'h4')
+      }
+      if (/^#####\s/.test(el.anchorNode.textContent)) {
+        document.execCommand('formatBlock', false, 'h5')
+      }
+      if (/^######\s/.test(el.anchorNode.textContent)) {
+        document.execCommand('formatBlock', false, 'h6')
+      }
     }
   }
 
   keydownHandler(e) {
     switch (e.keyCode) {
       case 13: // enter
-        e.preventDefault()
-        document.execCommand('insertHTML', false, '')
-        const newLine = document.createElement('div')
-        newLine.setAttribute('contenteditable', '')
-        newLine.classList.add('editor-input')
-        this.editorRef.current.appendChild(newLine)
-        newLine.innerHTML = ''
-        newLine.focus()
+        document.execCommand('removeFormat', false)
+        if (this.editorRef.current.firstChild.nodeType === 3) {
+          const o = this.editorRef.current.firstChild
+          const d = document.createElement('div')
+          d.textContent = o.textContent
+          this.editorRef.current.insertBefore(d, this.editorRef.current.firstChild)
+          this.editorRef.current.removeChild(o)
+        }
+        this.setState({ tree: this.editorRef.current.children })
         return false
       case 8: // delete
-        let currentSelection = window.getSelection()
-        const currentInput = currentSelection.anchorNode
-        if (
-          currentInput.textContent === '' ||
-          currentInput.textContent === ' '
-        ) {
-          e.preventDefault()
-          const previousLine = currentInput.previousElementSibling
-          if (previousLine) {
-            this.editorRef.current.removeChild(currentInput)
-            previousLine.focus()
-            // New range
-            const range = document.createRange()
-            range.selectNodeContents(previousLine)
-            range.collapse(false)
-            // Move the cursor
-            currentSelection = window.getSelection()
-            currentSelection.removeAllRanges()
-            currentSelection.addRange(range)
-          }
-          return false
-        }
         break
     }
   }
--- web/src/client/js/dashboard.js
+++ web/src/client/js/dashboard.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import EditorComponentContainer from '../components/EditorComponent'
 
-render(
-  <EditorComponentContainer />,
-  document.getElementById('editor')
-)
+render(<EditorComponentContainer />, document.getElementById('editor'))
+
+// Todo: Investigate why the globals.js doesnt work for this one
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept()
+}
