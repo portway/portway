00dbaee79330d673a66f207a50b917fe14161a9e
Jay Contonio <jay@conton.io>
2019-02-21 17:24:17 -0800

    Splitting up webpack config for more sharing

--- web/config/webpack.shared.js
+++ web/config/webpack.shared.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const WebpackAssetsManifest = require('webpack-assets-manifest')
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
 
 const SharedConfig = {
   resolvers: {
@@ -6,7 +9,83 @@ const SharedConfig = {
     Components: path.resolve(__dirname, '../src/client/js/components'),
     Shared: path.resolve(__dirname, '../src/shared'),
     CSS: path.resolve(__dirname, '../src/client/css')
-  }
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../src/client/images'),
+        to: 'images'
+      },
+      {
+        from: path.resolve(__dirname, '../src/client/manifest.webmanifest'),
+        to: 'manifest.webmanifest'
+      }
+    ]),
+    new WebpackAssetsManifest()
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+  styleLoaders: [
+    {
+      loader: 'css-loader',
+      options: {
+        sourceMap: true
+      }
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: true,
+        config: {
+          path: path.resolve(__dirname, './postcss.config.js')
+        }
+      }
+    },
+    {
+      loader: 'sass-loader',
+      options: {
+        sourceMap: true
+      }
+    }
+  ],
+  sharedLoaders: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.(png|jp(e*)g|svg)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }
+      ]
+    }
+  ]
 }
 
 module.exports = SharedConfig
