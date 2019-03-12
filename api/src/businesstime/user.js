e6a83603d7bf299cd5207d07a217e5779455dfb2
JJ Idt <johnidt@gmail.com>
2019-03-11 17:28:39 -0700

    set User constant in businesstime file

--- api/src/businesstime/user.js
+++ api/src/businesstime/user.js
@@ -1,8 +1,10 @@
 import { getDb } from '../db/db-connector'
 
+const MODEL_NAME = 'User'
+
 async function findByEmail(email) {
   const db = getDb()
-  const user = await db.model('User').findOne({ where: { email } })
+  const user = await db.model(MODEL_NAME).findOne({ where: { email } })
 
   return user && user.get({ plain: true })
 }
