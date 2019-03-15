f8f6a6c10232714543f57e3a1cc60d25d7ea1081
Jay Contonio <jay@conton.io>
2019-03-15 15:57:53 -0700

    Getting project just figured out

--- web/src/client/js/components/ProjectsList/ProjectsListItem.js
+++ web/src/client/js/components/ProjectsList/ProjectsListItem.js
@@ -6,13 +6,18 @@ import { Link } from 'react-router-dom'
 const ProjectsListItem = ({ projectId, project }) => {
   return (
     <li>
-      <div className="project-list__item" role="menu" tabIndex="0">
+      <div className="project-list__item project-list__item--disabled" role="menu" tabIndex="0">
         <div className="project-list__info">
           <span className="icon icon-project" />
-          <h3 className="project-list__title">{project.name}</h3>
-          <p className="project-list__item-meta">Last updated by _USER_</p>
+          <span>
+            <h3 className="project-list__title">{project.name}</h3>
+            <p className="project-list__item-meta">Last updated by _USER_</p>
+          </span>
         </div>
         <div className="project-list__actions">
+          <button className="btn btn--blank btn--warning">Delete</button>
+          <button className="btn btn--blank">Duplicate</button>
+          <button className="btn btn--blank">Settings</button>
           <Link to={`/project/${projectId}`} className="btn" role="button">
             Open
           </Link>
