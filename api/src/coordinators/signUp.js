429ca8db7a6cab1ad01bfec579e3dc9ed6bfc1c5
JJ Idt <johnidt@gmail.com>
2019-03-22 17:15:33 -0700

    generate unique reset key on signup

--- api/src/coordinators/signUp.js
+++ api/src/coordinators/signUp.js
@@ -1,3 +1,5 @@
+import uuidv1 from 'uuid/v1'
+
 import BusinessUser from '../businesstime/user'
 import BusinessOrganization from '../businesstime/organization'
 import tokenIntegrator from '../integrators/token'
@@ -5,17 +7,19 @@ import tokenIntegrator from '../integrators/token'
 async function signUp(firstName, lastName, email, password) {
   const organizationName = `${firstName} ${lastName}'s Organization`
   const organization = await BusinessOrganization.create({ name: organizationName })
+  const resetKey = uuidv1()
 
   const createdUser = await BusinessUser.create({
     firstName,
     lastName,
     email,
-    orgId: organization.id
+    orgId: organization.id,
+    resetKey
   })
 
   await BusinessOrganization.updateById(organization.id, { ownerId: createdUser.id })
 
-  const token = tokenIntegrator.generateToken(req.user.email, req.user.id)
+  const token = tokenIntegrator.generatePasswordResetToken(createdUser.id, resetKey)
   return token
 }
 
