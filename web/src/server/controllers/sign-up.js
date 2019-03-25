2a51d4eddad5dc94082c1ce8c7518e395855f073
JJ Idt <johnidt@gmail.com>
2019-03-25 13:05:50 -0700

    move controller code to declared functions

--- web/src/server/controllers/sign-up.js
+++ web/src/server/controllers/sign-up.js
@@ -10,60 +10,63 @@ const SignUpController = function(router) {
     res.render('user/processing', renderBundles(req, 'Processing', 'index'))
   })
 
-  router.post('/registration', auth.jwtMiddleware, async (req, res) => {
-    const { firstName, lastName, email } = req.body
-
-    let token
-    try {
-      ({ data: { token } } = await API.send({
-        url: 'signup',
-        method: 'POST',
-        data: {
-          firstName,
-          lastName,
-          email
-        }
-      }))
-    } catch ({ response }) {
-      console.error({ status: response.status, message: response.data })
-      return res.status(500).send('There was an error registering your organization')
-    }
-
-    res.redirect(`registration/password?token=${token}`)
-  })
+  router.post('/registration', auth.jwtMiddleware, registerOrganization)
 
   router.get('/registration/password', auth.jwtMiddleware, async (req, res) => {
     const { token } = req.query
-
     res.render('user/registration', { ...renderBundles(req, 'Registration', 'index'), token })
   })
 
-  router.post('/registration/password', auth.jwtMiddleware, async (req, res) => {
-    const { password, 'confirm-password': confirmPassword, token } = req.body
+  router.post('/registration/password', auth.jwtMiddleware, setInitialPassword)
+}
 
-    // TODO re render form with mismatch error message
-    if (password !== confirmPassword) {
-      return res.send(400)
-    }
+const registerOrganization = async (req, res) => {
+  const { firstName, lastName, email } = req.body
 
-    try {
-      await API.send({
-        url: 'signup/initialPassword',
-        method: 'POST',
-        headers: {
-          Authorization: `bearer ${token}`
-        },
-        data: {
-          password
-        }
-      })
-    } catch ({ response }) {
-      console.error({ status: response.status, message: response.data })
-      return res.status(500).send('There was an error setting your password')
-    }
+  let token
+  try {
+    ({ data: { token } } = await API.send({
+      url: 'signup',
+      method: 'POST',
+      data: {
+        firstName,
+        lastName,
+        email
+      }
+    }))
+  } catch ({ response }) {
+    console.error({ status: response.status, message: response.data })
+    return res.status(500).send('There was an error registering your organization')
+  }
 
-    res.redirect('/sign-in')
-  })
+  res.redirect(`registration/password?token=${token}`)
+}
+
+const setInitialPassword = async (req, res) => {
+  const { password, 'confirm-password': confirmPassword, token } = req.body
+
+  // TODO re render form with mismatch error message
+  if (password !== confirmPassword) {
+    return res.send(400)
+  }
+
+  try {
+    await API.send({
+      url: 'signup/initialPassword',
+      method: 'POST',
+      headers: {
+        Authorization: `bearer ${token}`
+      },
+      data: {
+        password
+      }
+    })
+  } catch ({ response }) {
+    console.error({ status: response.status, message: response.data })
+    return res.status(500).send('There was an error setting your password')
+  }
+
+  res.redirect('/sign-in')
 }
 
 export default SignUpController
