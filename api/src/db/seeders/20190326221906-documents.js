66b06539cf7f18c3ca104291092881a6668f4d06
Dirk Heniges <dheniges@gmail.com>
2019-03-27 08:00:13 -0700

    convert doc/field seeds to constant ids

--- api/src/db/seeders/20190326221906-documents.js
+++ api/src/db/seeders/20190326221906-documents.js
@@ -1,5 +1,8 @@
+const seedIds = require('../config/seedIds')
 
 const TODAY = new Date()
+const ORG_ID = seedIds.BONKEY_ORG_ID
+const BONKEY_PROJECT_ID = seedIds.BONKEY_PROJECT_ID
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -7,33 +10,33 @@ module.exports = {
       'Documents',
       [
         {
-          id: 888,
-          orgId: 1,
-          projectId: 1,
+          id: seedIds.BONKEY_DOC_1_ID,
+          orgId: ORG_ID,
+          projectId: BONKEY_PROJECT_ID,
           name: 'BonkeyDoc1',
           createdAt: TODAY,
           updatedAt: TODAY
         },
         {
-          id: 889,
-          orgId: 1,
-          projectId: 1,
+          id: seedIds.BONKEY_DOC_2_ID,
+          orgId: ORG_ID,
+          projectId: BONKEY_PROJECT_ID,
           name: 'BonkeyDoc2',
           createdAt: TODAY,
           updatedAt: TODAY
         },
         {
-          id: 890,
-          orgId: 1,
-          projectId: 1,
+          id: seedIds.BONKEY_DOC_3_ID,
+          orgId: ORG_ID,
+          projectId: BONKEY_PROJECT_ID,
           name: 'BonkeyDoc3',
           createdAt: TODAY,
           updatedAt: TODAY
         },
         {
-          id: 891,
-          orgId: 1,
-          projectId: 2,
+          id: seedIds.SCENIC_DOC_1_ID,
+          orgId: ORG_ID,
+          projectId: seedIds.SCENIC_PROJECT_ID,
           name: 'Scenic Trails Overview',
           createdAt: TODAY,
           updatedAt: TODAY
--- api/src/db/seeders/20190326222222-fields.js
+++ api/src/db/seeders/20190326222222-fields.js
@@ -1,22 +1,29 @@
+const seedIds = require('../config/seedIds')
+
 const TODAY = new Date()
+const ORG_ID = seedIds.BONKEY_ORG_ID
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Fields', [
-      {
-        name: 'Bonkey',
-        type: 1,
-        // eslint-disable-next-line no-multi-str
-        value: '# Wee Bonkey \
-        a strapping young lass \
-        **a heart of gold** \
-        _ah yes, wee bonkey_',
-        orgId: 1,
-        documentId: 888,
-        createdAt: TODAY,
-        updatedAt: TODAY
-      }
-    ], {})
+    return queryInterface.bulkInsert(
+      'Fields',
+      [
+        {
+          name: 'Bonkey',
+          type: 1,
+          // eslint-disable-next-line no-multi-str
+          value: '# Wee Bonkey \
+                  a strapping young lass \
+                  **a heart of gold** \
+                  _ah yes, wee bonkey_',
+          orgId: ORG_ID,
+          documentId: seedIds.BONKEY_DOC_1_ID,
+          createdAt: TODAY,
+          updatedAt: TODAY
+        }
+      ],
+      {}
+    )
   },
 
   down: (queryInterface, Sequelize) => {
