422f546435abded3f7862a2f47cfcbfe63b76fc1
Dirk Heniges <dheniges@gmail.com>
2019-04-08 09:02:10 -0700

    test

--- api/src/businesstime/projectuser.test-i.js
+++ api/src/businesstime/projectuser.test-i.js
@@ -1,26 +1,52 @@
-import BusinessProject from './project'
+import BusinessProjectUser from './projectuser'
 import ProjectFactory from '../db/__testSetup__/factories/project'
+import UserFactory from '../db/__testSetup__/factories/user'
 import initializeTestDb, { clearDb } from '../db/__testSetup__/initializeTestDb'
 import constants from '../db/__testSetup__/constants'
 
-describe('BusinessProject', () => {
+describe('BusinessProjectUser', () => {
   beforeAll(async () => {
     await initializeTestDb()
   })
 
-  describe('#findAllByProjectId', () => {
-    const projectBody = { name: 'test-project', orgId: constants.ORG_ID }
+  describe('#create', () => {
+    let user
     let project
+    let projectUserBody
+    let projectUser
 
     beforeAll(async () => {
-      project = await BusinessProject.create(projectBody)
+      project = await ProjectFactory.createMany(1, { orgId: constants.ORG_ID })[0]
+      user = await UserFactory.createMany(1, { orgId: constants.ORG_ID })[0]
+
+      projectUserBody = {
+        orgId: constants.ORG_ID,
+        userId: user.id,
+        projectId: project.id
+      }
+
+      projectUser = await BusinessProjectUser.create(projectUserBody)
     })
 
-    it('should return the saved project as a POJO', () => {
-      expect(project).toEqual(expect.objectContaining(projectBody))
-      expect(project.constructor).toBe(Object)
-      expect(project.orgId).toBe(constants.ORG_ID)
+    it('should create a project user assignment', () => {
+      expect(projectUser).toEqual(expect.objectContaining(projectUserBody))
+      expect(projectUser.constructor).toBe(Object)
     })
   })
+
+  // describe('#findAllByProjectId', () => {
+  //   const projectBody = { name: 'test-project', orgId: constants.ORG_ID }
+  //   let project
+
+  //   beforeAll(async () => {
+  //     projectuser = await BusinessProject.create(projectBody)
+  //   })
+
+  //   it('should return the saved project as a POJO', () => {
+  //     expect(project).toEqual(expect.objectContaining(projectBody))
+  //     expect(project.constructor).toBe(Object)
+  //     expect(project.orgId).toBe(constants.ORG_ID)
+  //   })
+  // })
 })
 
