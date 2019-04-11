31f7eda6113fd0a27b189660b21d3aab83e5ff07
JJ Idt <johnidt@gmail.com>
2019-04-10 17:38:46 -0700

    add comment in empty documents controller

--- api/src/controllers/documents.js
+++ api/src/controllers/documents.js
@@ -1,3 +1,4 @@
 const documentsController = function(router) {}
-
+// using this empty controller so that we can load the child field routes in loader.js off
+// of the base /documents route, actual document routes are in projectDocuments.js
 export default documentsController
