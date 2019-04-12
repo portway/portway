025c8f4f7399700569c7f0aada91b20a8395c805
JJ Idt <johnidt@gmail.com>
2019-04-11 17:23:11 -0700

    fix front end feching of data

--- web/src/client/js/actions/document.js
+++ web/src/client/js/actions/document.js
@@ -5,7 +5,7 @@ import { add, fetch, update } from '../api'
 export const fetchDocuments = (projectId) => {
   return async (dispatch) => {
     dispatch(Documents.requestList(projectId))
-    const data = await fetch(`projects/${projectId}/documents`)
+    const { data } = await fetch(`projects/${projectId}/documents`)
     dispatch(Documents.receiveList(projectId, data))
   }
 }
@@ -13,7 +13,7 @@ export const fetchDocuments = (projectId) => {
 export const fetchDocument = (projectId, documentId) => {
   return async (dispatch) => {
     dispatch(Documents.requestOne(projectId, documentId))
-    const data = await fetch(`projects/${projectId}/documents/${documentId}`)
+    const { data } = await fetch(`projects/${projectId}/documents/${documentId}`)
     dispatch(Documents.receiveOne(data))
   }
 }
@@ -21,7 +21,7 @@ export const fetchDocument = (projectId, documentId) => {
 export const createDocument = (projectId, history, body) => {
   return async (dispatch) => {
     dispatch(Documents.create(projectId, body))
-    const data = await add(`projects/${projectId}/documents`, body)
+    const { data } = await add(`projects/${projectId}/documents`, body)
     dispatch(Documents.receiveOneCreated(data))
     history.push({ pathname: `${Constants.PATH_PROJECT}/${projectId}/document/${data.id}` })
   }
@@ -30,7 +30,7 @@ export const createDocument = (projectId, history, body) => {
 export const updateDocument = (documentId, projectId, body) => {
   return async (dispatch) => {
     dispatch(Documents.update(documentId))
-    const data = await update(`projects/${projectId}/documents/${documentId}`, body)
+    const { data } = await update(`projects/${projectId}/documents/${documentId}`, body)
     dispatch(Documents.receiveOneUpdated(data))
   }
 }
