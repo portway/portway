df02cba617f74e571c507398af3749d6788275c6
Dirk Heniges <dheniges@gmail.com>
2019-04-17 08:41:33 -0700

    project public fields

--- api/src/constants/resourcePublicFields.js
+++ api/src/constants/resourcePublicFields.js
@@ -4,6 +4,9 @@ import globalPublicFields from './globalPublicFields'
 
 const DOCUMENT_PUBLIC_FIELDS = ['name', 'publishedVersionId', 'projectId'].concat(globalPublicFields)
 
+const PROJECT_PUBLIC_FIELDS = ['name', 'description'].concat(globalPublicFields)
+
 export default {
-  [resourceTypes.DOCUMENT]: DOCUMENT_PUBLIC_FIELDS
+  [resourceTypes.DOCUMENT]: DOCUMENT_PUBLIC_FIELDS,
+  [resourceTypes.PROJECT]: PROJECT_PUBLIC_FIELDS
 }
