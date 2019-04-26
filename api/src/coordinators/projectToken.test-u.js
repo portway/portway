e9be267447d3462cc54e9c404da9b29ff4318830
Dirk Heniges <dheniges@gmail.com>
2019-04-26 11:14:00 -0700

    more project token tests

--- api/src/coordinators/projectToken.test-u.js
+++ api/src/coordinators/projectToken.test-u.js
@@ -1,4 +1,5 @@
-import { createProjectToken } from './projectToken'
+import jsonwebtoken from 'jsonwebtoken'
+import { createProjectToken, verifyProjectToken } from './projectToken'
 import randomStringGenerator from '../libs/randomStringGenerator'
 import tokenGenerator from '../integrators/token'
 import BusinessProjectToken from '../businesstime/projecttoken'
@@ -8,6 +9,7 @@ import resourcePublicFields from '../constants/resourcePublicFields'
 jest.mock('../businesstime/projecttoken')
 jest.mock('../integrators/token')
 jest.mock('../libs/randomStringGenerator')
+jest.mock('jsonwebtoken')
 
 describe('projectToken coordinator', () => {
   const name = 'Project Token Name'
@@ -42,6 +44,42 @@ describe('projectToken coordinator', () => {
 
     it('should return a token', () => {
       expect(Object.keys(token)).toEqual(expect.arrayContaining(resourcePublicFields[resourceTypes.PROJECT_TOKEN]))
+    })
+  })
+
+  describe('#verifyProjectToken', () => {
+    let verifiedToken
+    const decodedToken = {
+      id: 123,
+      orgId: 456
+    }
+    const tokenString = 'this-token-is-fake'
+
+    beforeAll(async () => {
+      jsonwebtoken.decode.mockReturnValue(decodedToken)
+      jsonwebtoken.verify.mockImplementation((token, secret, options, callback) => {
+        callback(null, decodedToken)
+      })
+      verifiedToken = await verifyProjectToken(tokenString)
+    })
+
+    it('should return the token', () => {
+      expect(Object.keys(verifiedToken)).toEqual(expect.arrayContaining(['id', 'token', 'name']))
+    })
+
+    it('should find unsanitized token by id', () => {
+      expect(BusinessProjectToken.findByIdUnsanitized.mock.calls.length).toBe(1)
+      expect(BusinessProjectToken.findByIdUnsanitized.mock.calls[0][0]).toBe(decodedToken.id)
+    })
+
+    it('should decode jwt', () => {
+      expect(jsonwebtoken.decode.mock.calls.length).toBe(1)
+      expect(jsonwebtoken.decode.mock.calls[0][0]).toBe(tokenString)
+    })
+
+    it('should verify jwt', () => {
+      expect(jsonwebtoken.verify.mock.calls.length).toBe(1)
+      expect(jsonwebtoken.verify.mock.calls[0][0]).toBe(tokenString)
     })
   })
 })
