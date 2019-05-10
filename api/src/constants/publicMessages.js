469289decad6b92c5ea95a27de0c8f9f508c5a7e
JJ Idt <johnidt@gmail.com>
2019-05-10 12:32:51 -0700

    use error middleware for validation

--- api/src/constants/publicMessages.js
+++ api/src/constants/publicMessages.js
@@ -1,3 +1,6 @@
 export default {
-  NOT_FOUND: 'Not Found'
+  NOT_FOUND: 'Not Found',
+  INVALID_PAYLOAD: 'Invalid Request Payload Input',
+  INVALID_PARAM: 'Invalid Request Url Parameter',
+  SERVER_ERROR: 'Server Error'
 }
