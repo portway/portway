a56655efa61e579a2cbdaa0fddc2d5bc4542a93c
JJ Idt <johnidt@gmail.com>
2019-05-17 12:12:42 -0700

    handle bodyParser errors

--- api/src/libs/middleware/apiErrorHandler.js
+++ api/src/libs/middleware/apiErrorHandler.js
@@ -3,7 +3,7 @@ import PUBLIC_MESSAGES from '../../constants/publicMessages'
 const getPublicMessage = function(code) {
   switch (code) {
     case 400:
-      return PUBLIC_MESSAGES.VALIDATION_ERROR
+      return PUBLIC_MESSAGES.BAD_REQUEST
     case 404:
       return PUBLIC_MESSAGES.NOT_FOUND
     case 500:
@@ -12,12 +12,37 @@ const getPublicMessage = function(code) {
   }
 }
 
+// This final piece of middleware handles any errors passed through the middleware chain
+// these could have one of a few different formats
+// for instance, bodyParser errors will look like:
+// { status: 400, statusCode: 400, expose: true, message: 'Unexpected token \n in JSON at position 9' }
+// while our own errors will look like:
+// { code: 404, publicMessage: 'Not Found', message: 'Some internal message not for public consumption' }
+// and some potentially unhandled internal errors could just be:
+// { message: 'Some internal error' }
+
 export default function(error, req, res, next) {
-  const { code = 500, errorType, errorDetails, publicMessage } = error
+  const {
+    code,
+    status,
+    statusCode,
+    expose,
+    errorType,
+    errorDetails,
+    message,
+    publicMessage } = error
+
+  const responseCode = code || status || statusCode || 500
+
+  // some middleware will attach an expose Boolean to the error, which is a green-light to pass the error message directly to consumer
+  const exposedMessage = expose ? message : null
+
+  const responseMessage = publicMessage || exposedMessage || getPublicMessage(responseCode)
+
   //TODO handle conditional logging here for different environments
   console.error(error.stack)
-  res.status(code).json({
-    error: publicMessage || getPublicMessage(code),
+  res.status(responseCode).json({
+    error: responseMessage,
     errorType,
     errorDetails
   })
