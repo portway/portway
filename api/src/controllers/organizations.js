064f482e486b1f8b6b644821c2c05204eea91796
JJ Idt <johnidt@gmail.com>
2019-05-21 11:23:34 -0700

    add READ_MY perm for organization

--- api/src/controllers/organizations.js
+++ api/src/controllers/organizations.js
@@ -1,15 +1,44 @@
 import Joi from 'joi'
-import { validateBody } from '../libs/middleware/payloadValidation'
-import BusinessOrganization from '../businesstime/organization'
 import ono from 'ono'
+
+import { validateBody, validateParams } from '../libs/middleware/payloadValidation'
+import BusinessOrganization from '../businesstime/organization'
+import perms from '../libs/middleware/reqPermissionsMiddleware'
+import crudPerms from '../libs/middleware/reqCrudPerms'
+import RESOURCE_TYPES from '../constants/resourceTypes'
+import ACTIONS from '../constants/actions'
+
+const paramSchema = Joi.compile({
+  id: Joi.number().required()
+})
 
 const organizationsPayloadSchema = Joi.compile({
   name: Joi.string().required()
 })
 
+const { readPerm } = crudPerms(
+  RESOURCE_TYPES.ORGANIZATION,
+  (req) => { return { id: req.params.id } }
+)
+
+const conditionalReadPerm = (req, res, next) => {
+  const { id } = req.params
+  // if ids match use the 'READ_MY' user perm
+  if (id === req.requestorInfo.orgId) {
+    return perms((req) => {
+      return {
+        resourceType: RESOURCE_TYPES.ORGANIZATION,
+        action: ACTIONS.READ_MY
+      }
+    })(req, res, next)
+  }
+  // not requesting self, normal organization read perm
+  return readPerm(req, res, next)
+}
+
 const organizationsController = function(router) {
   router.post('/', validateBody(organizationsPayloadSchema), addOrganization)
-  router.get('/:id', getOrganization)
+  router.get('/:id', validateParams(paramSchema), conditionalReadPerm, getOrganization)
 }
 
 const getOrganization = async function(req, res, next) {
