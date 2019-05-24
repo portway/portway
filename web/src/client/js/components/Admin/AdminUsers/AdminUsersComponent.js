e889f5333e23eb974b05145a5d599736a9ea7511
Jay Contonio <jay@conton.io>
2019-05-24 12:11:01 -0700

    Start of table component

--- web/src/client/js/components/Admin/AdminUsers/AdminUsersComponent.js
+++ web/src/client/js/components/Admin/AdminUsers/AdminUsersComponent.js
@@ -3,20 +3,40 @@ import PropTypes from 'prop-types'
 
 import { UserIcon } from 'Components/Icons'
 import ToolbarComponent from 'Components/Toolbar/ToolbarComponent'
+import Table from 'Components/Table/Table'
 
 const AdminUsersComponent = ({ users }) => {
-  console.log(users)
   const toolbarAction = {
     callback: () => {},
     icon: <UserIcon />,
     label: 'Add User',
     title: 'Add a new user',
   }
+
+  const userHeadings = {
+    name: { label: 'Name', sortable: true },
+    email: { label: 'Email', sortable: false },
+    role: { label: 'Role', sortable: true },
+    createdAt: { label: 'Added', sortable: true }
+  }
+
+  // Create a nice user row object
+  const userRows = {}
+  Object.values(users).forEach((user) => {
+    userRows[user.id] = [
+      user.name,
+      user.email,
+      user.orgRoleId,
+      user.createdAt
+    ]
+  })
+
   return (
     <div>
       <section>
         <h2>User Management</h2>
         <ToolbarComponent action={toolbarAction} filter sort />
+        <Table headings={userHeadings} rows={userRows} />
       </section>
     </div>
   )
--- web/src/client/js/components/Table/Table.js
+++ web/src/client/js/components/Table/Table.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Table = ({ children }) => {
+import TableHeading from './TableHeading'
+import TableRow from './TableRow'
+
+const Table = ({ headings, rows }) => {
+  function renderHeadings() {
+    return Object.keys(headings).map((hId) => {
+      return <TableHeading key={hId} sortable={headings[hId].sortable}>{headings[hId].label}</TableHeading>
+    })
+  }
+
+  function renderRows() {
+    return Object.keys(rows).map((rId) => {
+      return <TableRow key={rId} cells={rows[rId]} />
+    })
+  }
+
   return (
-    <table>
-      {children}
-    </table>
+    <section className="table">
+      <ol className="table__list">
+        <li className="table__heading">
+          {renderHeadings()}
+        </li>
+        {renderRows()}
+      </ol>
+    </section>
   )
 }
 
+// @todo Shape this up
 Table.propTypes = {
-  children: PropTypes.node
+  headings: PropTypes.object.isRequired,
+  rows: PropTypes.object.isRequired
+}
+
+Table.defaultProps = {
+  sortable: false
 }
 
 export default Table
