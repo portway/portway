51544712ce134448746c9f0413356861b4ffd85c
JJ Idt <johnidt@gmail.com>
2019-08-20 16:26:30 -0700

    functioning s3 image upload integrator

--- api/src/businesstime/field.js
+++ api/src/businesstime/field.js
@@ -8,7 +8,7 @@ import apiErrorTypes from '../constants/apiErrorTypes'
 import resourceTypes from '../constants/resourceTypes'
 import resourcePublicFields from '../constants/resourcePublicFields'
 import { pick } from '../libs/utils'
-import { uploadFile } from '../integrators/s3'
+import { uploadImage } from '../integrators/s3'
 
 const MODEL_NAME = 'Field'
 
@@ -18,7 +18,7 @@ const publicFields = (instance) => {
   return pick(instance, PUBLIC_FIELDS)
 }
 
-async function createForDocument(docId, body) {
+async function createForDocument(docId, body, file) {
   const db = getDb()
   const { orgId } = body
 
@@ -44,8 +44,7 @@ async function createForDocument(docId, body) {
   let value
 
   if (createFieldBody.type === FIELD_TYPES.IMAGE) {
-    const url = uploadFile(file)
-    console.log(url)
+    const url = await uploadImage(file)
     value = url
   } else {
     value = body.value
--- api/src/integrators/s3.js
+++ api/src/integrators/s3.js
@@ -1,6 +1,5 @@
 import AWS from 'aws-sdk'
 import ono from 'ono'
-import mime from 'mime-types'
 
 const { S3_IMAGE_BUCKET, AWS_SES_REGION } = process.env
 
@@ -8,16 +7,18 @@ AWS.config.update({ region: AWS_SES_REGION })
 
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
 
-export const uploadFile = async function(file) {
-  const type = mime.lookup(file.path)
-
+export const uploadImage = async function(file) {
   let res
   try {
-    res = await s3.putObject({
-      Bucket: S3_IMAGE_BUCKET,
-      Key: Date.now(),
-      ContentType: type
-    })
+    res = await s3
+      .upload({
+        Bucket: S3_IMAGE_BUCKET,
+        Key: file.originalname,
+        ContentType: 'application/octet-stream',
+        Body: file.buffer,
+        ACL: 'public-read'
+      })
+      .promise()
   } catch (err) {
     throw ono(err, { code: 503 }, 'AWS s3 failed to upload file')
   }
