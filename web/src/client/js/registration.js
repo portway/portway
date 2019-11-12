d61bbd92977a3c3fe626155a87ce7859e95502c9
Jay Contonio <jay@conton.io>
2019-11-12 11:49:45 -0800

    Fixing password validation on reg

--- web/src/client/js/registration.js
+++ web/src/client/js/registration.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
 import zxcvbn from 'zxcvbn'
 
-import { debounce } from 'Shared/utilities'
 import PopoverComponent from 'Components/Popover/PopoverComponent'
 
 import 'CSS/registration.scss'
@@ -13,6 +12,8 @@ const confirmField = document.querySelector('#confirm-password')
 const pwStatus = document.querySelector('#pw-status')
 const coStatus = document.querySelector('#co-status')
 
+let passwordFieldSuccess = false
+
 function isPasswordMatch() {
   return passwordField.value !== '' && passwordField.value === confirmField.value
 }
@@ -21,12 +22,24 @@ function isRegReady() {
   return isPasswordMatch()
 }
 
-const passwordScoreHandler = debounce(500, (e) => {
+const resetPasswordStatus = (e) => {
+  setTimeout(() => {
+    unmountComponentAtNode(document.getElementById('pw-messages'))
+    unmountComponentAtNode(document.getElementById('co-messages'))
+    passwordField.removeEventListener('keydown', resetPasswordStatus, false)
+  }, 1000)
+}
+
+const passwordScoreHandler = (e) => {
   const pw = e.target.value
   const result = zxcvbn(pw)
   // Scores are 0 - 4
   if (result.score < 3) {
-    confirmField.setAttribute('disabled', true)
+    // Focus the input, and clear the popover once we start typing
+    setTimeout(() => { passwordField.focus() }, 100)
+    passwordFieldSuccess = false
+    passwordField.addEventListener('keydown', resetPasswordStatus, false)
+    pwStatus.style.display = 'none'
     const Popover = (
       <PopoverComponent name="password-tip">
         <p>{result.feedback.warning}</p>
@@ -42,36 +55,40 @@ const passwordScoreHandler = debounce(500, (e) => {
     render(Popover, document.getElementById('pw-messages'))
     return
   }
-  confirmField.removeAttribute('disabled')
   unmountComponentAtNode(document.getElementById('pw-messages'))
+  passwordFieldSuccess = true
   pwStatus.style.display = 'block'
-})
+}
 
-const confirmPasswordHandler = debounce(200, (e) => {
+const confirmPasswordHandler = (e) => {
   if (isPasswordMatch()) {
-    confirmField.classList.remove('is-danger')
     unmountComponentAtNode(document.getElementById('co-messages'))
     coStatus.style.display = 'block'
     if (isRegReady()) {
       submitBtn.removeAttribute('disabled')
     }
   } else {
-    const Popover = (
-      <PopoverComponent name="confirm-tip">
-        <p>Your password’s do not match.</p>
-      </PopoverComponent>
-    )
-    render(Popover, document.getElementById('co-messages'))
-    coStatus.style.display = 'none'
-    submitBtn.setAttribute('disabled', true)
-    return
+    if (passwordFieldSuccess) {
+      // Focus the input, and clear the popover once we start typing
+      setTimeout(() => { confirmField.focus() }, 100)
+      confirmField.addEventListener('keydown', resetPasswordStatus, false)
+      coStatus.style.display = 'none'
+      submitBtn.setAttribute('disabled', true)
+      const Popover = (
+        <PopoverComponent name="confirm-tip">
+          <p>Your password’s do not match.</p>
+        </PopoverComponent>
+      )
+      render(Popover, document.getElementById('co-messages'))
+      return
+    }
   }
-})
+}
 
 // Disable the submit button unless we know the org name has a value
 submitBtn.setAttribute('disabled', true)
-passwordField.addEventListener('keyup', passwordScoreHandler, false)
-confirmField.addEventListener('keyup', confirmPasswordHandler, false)
+passwordField.addEventListener('blur', passwordScoreHandler, false)
+confirmField.addEventListener('blur', confirmPasswordHandler, false)
 
 // Todo: Investigate why the globals.js doesnt work for this one
 if (process.env.NODE_ENV !== 'production' && module.hot) {
