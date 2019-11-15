b614c60a21cd2e95df30b0cd116543f292e16893
JJ Idt <johnidt@gmail.com>
2019-11-15 15:30:05 -0800

    stripe publishable key via env

--- web/src/server/controllers/app.js
+++ web/src/server/controllers/app.js
@@ -4,7 +4,7 @@ const AppController = function(router) {
   // Sending all requests to dashboard view, for Redux Router
   router.get('*', (req, res) => {
     const bundleTemplateVars = renderBundles(req, 'Welcome', 'app')
-    res.render('app/index', { ...bundleTemplateVars, apiPublicUrl: process.env.API_PUBLIC_URL } )
+    res.render('app/index', { ...bundleTemplateVars, apiPublicUrl: process.env.API_PUBLIC_URL, stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY } )
   })
 }
 
