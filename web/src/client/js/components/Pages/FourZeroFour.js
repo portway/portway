28991b4f99afd3b5e3f90c74f7e5cd484d2dd412
Jay Contonio <jay@conton.io>
2019-12-18 12:08:30 -0800

    Updating 404 styles

--- web/src/client/js/components/Pages/FourZeroFour.js
+++ web/src/client/js/components/Pages/FourZeroFour.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
+import { PATH_PROJECTS, SUPPORT_EMAIL } from 'Shared/constants'
+import { SearchIcon } from 'Components/Icons'
 
 const FourZeroFour = () => {
+  const location = useLocation()
   return (
-    <div><h1>404</h1></div>
+    <main>
+      <section>
+        <div className="notice">
+          <div className="notice__icon">
+            <SearchIcon />
+          </div>
+          <h2 className="notice__headline">Not found...</h2>
+          <p>We couldn’t find anything at “{location.pathname}”.</p>
+          <p className="small">Think this is a mistake? <a href={`mailto:${SUPPORT_EMAIL}`}>Contact support</a>.</p>
+          <Link to={PATH_PROJECTS} className="btn btn--small notice__action">Return to projects</Link>
+        </div>
+      </section>
+    </main>
   )
 }
 
