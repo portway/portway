2bca26579233683eaee28086e36e1cf066dd59d3
Jay Contonio <jay@conton.io>
2020-01-07 10:44:32 -0800

    Adding project deletion to project settings

--- web/src/client/js/components/ProjectSettings/ProjectSettingsInfo/ProjectSettingsInfoContainer.js
+++ web/src/client/js/components/ProjectSettings/ProjectSettingsInfo/ProjectSettingsInfoContainer.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 
-import { updateProject } from 'Actions/project'
+import { removeProject, updateProject } from 'Actions/project'
+import { uiConfirm } from 'Actions/ui'
 import useDataService from 'Hooks/useDataService'
 import currentResource from 'Libs/currentResource'
 
 import { PRODUCT_NAME } from 'Shared/constants'
 import ProjectSettingsInfoComponent from './ProjectSettingsInfoComponent'
 
-const ProjectSettingsInfoContainer = ({ errors, location, updateProject }) => {
+const ProjectSettingsInfoContainer = ({ errors, removeProject, uiConfirm, updateProject }) => {
+  const history = useHistory()
+  const location = useLocation()
+
   const { data: project } = useDataService(currentResource('project', location.pathname), [
     location.pathname
   ])
@@ -19,6 +23,22 @@ const ProjectSettingsInfoContainer = ({ errors, location, updateProject }) => {
   if (!project) return null
 
   const formId = 'project-settings'
+
+  const message = (
+    <>
+      <p className="danger">Delete this project?</p>
+      <p>This will remove all of the documents, assets, and other media related to this project.</p>
+    </>
+  )
+
+  function deleteProjectHandler() {
+    const confirmedAction = () => {
+      removeProject(project.id, history)
+    }
+    const confirmedLabel = 'Yes, delete this project'
+    const confirmedText = project.title
+    uiConfirm({ message, confirmedAction, confirmedLabel, confirmedText })
+  }
 
   const debouncedUpdateHandler = (body) => {
     updateProject(formId, project.id, body)
@@ -29,14 +49,21 @@ const ProjectSettingsInfoContainer = ({ errors, location, updateProject }) => {
       <Helmet>
         <title>{project.name}: Information –– {PRODUCT_NAME}</title>
       </Helmet>
-      <ProjectSettingsInfoComponent errors={errors} formId={formId} project={project} updateProjectHandler={debouncedUpdateHandler} />
+      <ProjectSettingsInfoComponent
+        deleteProjectHandler={deleteProjectHandler}
+        errors={errors}
+        formId={formId}
+        project={project}
+        updateProjectHandler={debouncedUpdateHandler}
+      />
     </>
   )
 }
 
 ProjectSettingsInfoContainer.propTypes = {
   errors: PropTypes.object,
-  location: PropTypes.object.isRequired,
+  removeProject: PropTypes.func.isRequired,
+  uiConfirm: PropTypes.func.isRequired,
   updateProject: PropTypes.func.isRequired,
 }
 
@@ -46,8 +73,6 @@ const mapStateToProps = (state) => {
   }
 }
 
-const mapDispatchToProps = { updateProject }
+const mapDispatchToProps = { removeProject, uiConfirm, updateProject }
 
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(ProjectSettingsInfoContainer)
-)
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectSettingsInfoContainer)
