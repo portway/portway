cf185b7d059f142cecd2d05a1aa9cd6367d07575
JJ Idt <johnidt@gmail.com>
2020-01-07 09:27:27 -0800

    return updated organization

--- api/src/coordinators/organization.js
+++ api/src/coordinators/organization.js
@@ -5,11 +5,13 @@ const updateById = async function(id, body) {
   const currentOrg = await BusinessOrganization.findById(id)
   const stripeCustomer = await stripeIntegrator.getCustomer(currentOrg.stripeId)
 
-  await BusinessOrganization.updateById(id, body)
+  const updatedOrganization = await BusinessOrganization.updateById(id, body)
 
   if (stripeCustomer && body.name && body.name !== currentOrg.name) {
     await stripeIntegrator.updateCustomer(stripeCustomer.id, { name: body.name })
   }
+
+  return updatedOrganization
 }
 
 export default {
