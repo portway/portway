2dfd712214ba3aa9aeb548bf73f775af73d367f7
Dirk Heniges <dheniges@gmail.com>
2020-01-21 11:46:13 -0800

    add logging to orgRateLimiter if rate limiter errors out

--- api/src/libs/middleware/orgRateLimiter.js
+++ api/src/libs/middleware/orgRateLimiter.js
@@ -1,6 +1,9 @@
 import ono from 'ono'
 import rateLimiter from '../rateLimiter'
 import PUBLIC_MESSAGES from '../../constants/publicMessages'
+import logger from '../../integrators/logger'
+import { LOG_LEVELS } from '../../constants/logging'
+
 
 // Relies on reqInfoExtractorMiddleware
 // that middleware MUST run first to set orgId on the request
@@ -9,7 +12,7 @@ export default async function orgRateLimiter(req, res, next) {
   try {
     underRateLimit = await rateLimiter(req.requestorInfo.orgId)
   } catch (e) {
-    // TODO: log error
+    logger(LOG_LEVELS.WARNING, e)
     return next() // let request proceed if rateLimiter is down
   }
 
@@ -18,7 +21,7 @@ export default async function orgRateLimiter(req, res, next) {
   } else {
     const rateLimitError = new ono(
       { code: 409, publicMessage: PUBLIC_MESSAGES.RATE_LIMIT_EXCEEDED },
-      'Rate Limit Exceeded'
+      `Rate Limit Exceeded for org ${req.requestorInfo.orgId}`
     )
     return next(rateLimitError)
   }
