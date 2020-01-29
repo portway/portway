b01390f3d5d8564feedc805ca0f811f6c512cc17
JJ Idt <johnidt@gmail.com>
2020-01-29 09:48:01 -0800

    add canceled org functions to org coordinator

--- api/src/businesstime/organization.js
+++ api/src/businesstime/organization.js
@@ -4,7 +4,7 @@ import { getDb } from '../db/dbConnector'
 import resourcePublicFields from '../constants/resourcePublicFields'
 import resourceTypes from '../constants/resourceTypes'
 import { pick } from '../libs/utils'
-
+import { Op } from 'sequelize'
 
 const MODEL_NAME = 'Organization'
 const PUBLIC_FIELDS = resourcePublicFields[resourceTypes.ORGANIZATION]
@@ -65,11 +65,23 @@ async function deleteById(id) {
   return db.model(MODEL_NAME).destroy({ where: { id } })
 }
 
+async function findAllCanceled() {
+  const db = getDb()
+  return db.model(MODEL_NAME).find({
+    where: {
+      canceledAt: {
+        [Op.ne]: null
+      }
+    }
+  })
+}
+
 export default {
   create,
   findSanitizedById,
   updateById,
   findById,
   findByStripeId,
-  deleteById
+  deleteById,
+  findAllCanceled
 }
