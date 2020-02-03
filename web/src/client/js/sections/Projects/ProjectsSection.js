2f280d9cdee48b474b883350433d0884ba69a891
Jay Contonio <jay@conton.io>
2020-02-03 15:44:56 -0800

    Fixing project list scrolling

--- web/src/client/js/sections/Projects/ProjectsSection.js
+++ web/src/client/js/sections/Projects/ProjectsSection.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom'
 
@@ -10,30 +10,34 @@ import OrgPermission from 'Components/Permission/OrgPermission'
 
 const { ORGANIZATION_ROLE_IDS, ORGANIZATION_SETTINGS, PATH_PROJECT_CREATE } = Constants
 
-class ProjectsContainer extends React.PureComponent {
-  render() {
-    const toolbarAction = {
-      callback: () => {
-        this.props.history.push({ pathname: PATH_PROJECT_CREATE })
-      },
-      label: `New Project`,
-      icon: <AddIcon width="12" height="12" />,
-      title: 'New Project'
+const ProjectsContainer = () => {
+  useEffect(() => {
+    document.querySelector('body').classList.add('body--with-scrolling')
+    return function cleanup() {
+      document.querySelector('body').classList.remove('body--with-scrolling')
     }
-    return (
-      <main>
-        <OrgPermission
-          acceptedRoleIds={[ORGANIZATION_ROLE_IDS.OWNER, ORGANIZATION_ROLE_IDS.ADMIN]}
-          acceptedSettings={[ORGANIZATION_SETTINGS.ALLOW_USER_PROJECT_CREATION]}
-          elseRender={(
-            <ToolbarComponent action={{}} />
-          )}>
-          <ToolbarComponent action={toolbarAction} />
-        </OrgPermission>
-        <ProjectsListContainer />
-      </main>
-    )
+  }, [])
+  const toolbarAction = {
+    callback: () => {
+      this.props.history.push({ pathname: PATH_PROJECT_CREATE })
+    },
+    label: `New Project`,
+    icon: <AddIcon width="12" height="12" />,
+    title: 'New Project'
   }
+  return (
+    <main>
+      <OrgPermission
+        acceptedRoleIds={[ORGANIZATION_ROLE_IDS.OWNER, ORGANIZATION_ROLE_IDS.ADMIN]}
+        acceptedSettings={[ORGANIZATION_SETTINGS.ALLOW_USER_PROJECT_CREATION]}
+        elseRender={(
+          <ToolbarComponent action={{}} />
+        )}>
+        <ToolbarComponent action={toolbarAction} />
+      </OrgPermission>
+      <ProjectsListContainer />
+    </main>
+  )
 }
 
 ProjectsContainer.propTypes = {
