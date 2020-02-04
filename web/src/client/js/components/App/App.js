13cbfba2f7150ec5600094d109440feff8ea043f
Jay Contonio <jay@conton.io>
2020-02-03 16:13:41 -0800

    Adding orientation detect

--- web/src/client/js/components/App/App.js
+++ web/src/client/js/components/App/App.js
@@ -1,20 +1,30 @@
 import { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { debounce } from 'Shared/utilities'
 
 import 'CSS/app.scss'
 
 const App = ({ children, history, routeChange }) => {
   useEffect(() => {
     const appElement = document.getElementById('application')
-    function resizeHander() {
+
+    // Set the application div to the height of the device or window
+    const resizeHander = debounce(400, () => {
       appElement.style.height = window.innerHeight + 'px'
-    }
-    resizeHander()
+    })
+
+    window.addEventListener('orientationchange', resizeHander, false)
     window.addEventListener('resize', resizeHander, false)
+
     history.listen((location, action) => {
       routeChange()
     })
+
+    // Call it on load
+    resizeHander()
+
     return function cleanup() {
+      window.removeEventListener('orientationchange', resizeHander, false)
       window.removeEventListener('resize', resizeHander, false)
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
