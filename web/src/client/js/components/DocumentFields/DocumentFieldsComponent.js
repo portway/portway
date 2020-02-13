168858949135b7cbaacfc08dfbe179b221b435b6
Jay Contonio <jay@conton.io>
2020-02-13 12:49:49 -0800

    Removing log

--- web/src/client/js/components/DocumentFields/DocumentFieldsComponent.js
+++ web/src/client/js/components/DocumentFields/DocumentFieldsComponent.js
@@ -24,8 +24,6 @@ const DocumentFieldsComponent = ({
 }) => {
   const [settingsForField, setSettingsForField] = useState(null)
 
-  console.log(fields)
-
   const textFields = fields.filter((field) => {
     return field.type === FIELD_TYPES.TEXT
   })
