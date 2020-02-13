4f49fbaa867721734349ced0fddf6a058f44a9ef
JJ Idt <johnidt@gmail.com>
2020-02-12 17:58:02 -0800

    add disconnect event

--- sync/src/libs/redis.js
+++ sync/src/libs/redis.js
@@ -5,7 +5,7 @@ const REDIS_PORT = '6379'
 const redis = new Redis(REDIS_PORT)
 
 redis.on('connect', () => {
-  console.log(`redis connected on port ${REDIS_PORT}`)
+  console.info(`redis connected on port ${REDIS_PORT}`)
 })
 
 export default redis
--- sync/src/socketControllers/documents.js
+++ sync/src/socketControllers/documents.js
@@ -4,17 +4,23 @@ export default (io) => {
   const documentsIO = io.of('/documents')
 
   documentsIO.on('connection', (socket) => {
-    socket.on('joinRoom', async (documentId, userId) => {
+    const userId = socket.handshake.query.userId
+
+    socket.on('joinRoom', async (documentId) => {
       socket.join(documentId)
       // add user to document room Set
       const docRoomNs = `docRoom:${documentId}`
       await redis.sadd(docRoomNs, userId)
+      // add room to user rooms Set
+      const userRoomsNs = getUserRoomsNs(userId)
+      await redis.sadd(userRoomsNs, documentId)
+
       const currentRoomUsers = await redis.smembers(docRoomNs)
       // send user change message to all room users, including current user
       documentsIO.in(documentId).emit('userChange', currentRoomUsers)
     })
 
-    socket.on('leaveRoom', async (documentId, userId) => {
+    socket.on('leaveRoom', async (documentId) => {
       socket.leave(documentId)
       // remove user from document room Set
       const docRoomNs = getDocRoomNs(documentId)
@@ -23,9 +29,19 @@ export default (io) => {
       // send user change message to all room users, including current user
       documentsIO.in(documentId).emit('userChange', currentRoomUsers)
     })
+
+    socket.on('disconnect', async (data) => {
+      console.log(`${userId} disconnected`)
+      // send user change message to all room users, including current user
+      // documentsIO.emit('someone left')
+    })
   })
 }
 
 const getDocRoomNs = (documentId) => {
   return `docRoom:${documentId}`
+}
+
+const getUserRoomsNs = (userId) => {
+  return `userDocRooms:${userId}`
 }
