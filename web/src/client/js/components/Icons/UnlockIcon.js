66ab9561c67ad1a0828380589b506dbf374060ce
Jay Contonio <jay@conton.io>
2020-02-13 16:05:19 -0800

    Changing icon theming

--- web/src/client/js/components/Icons/UnlockIcon.js
+++ web/src/client/js/components/Icons/UnlockIcon.js
@@ -21,7 +21,7 @@ UnlockIcon.propTypes = {
 
 UnlockIcon.defaultProps = {
   className: 'icon-unlock',
-  fill: window.matchMedia('(prefers-color-scheme: dark)').matches ? '#F2F2F2' : '#3b3d3e',
+  fill: 'var(--theme-icon-color)',
   height: '18',
   width: '18'
 }
