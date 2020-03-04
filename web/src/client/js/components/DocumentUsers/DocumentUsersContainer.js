77c02eef31d07c3b342a1279ef5725a727bd454e
Jay Contonio <jay@conton.io>
2020-03-03 17:47:21 -0800

    Fetching unloaded users

--- web/src/client/js/components/DocumentUsers/DocumentUsersContainer.js
+++ web/src/client/js/components/DocumentUsers/DocumentUsersContainer.js
@@ -1,20 +1,29 @@
 import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
+import { fetchUsersWithIds } from 'Actions/user'
 import useDocumentSocket from 'Hooks/useDocumentSocket'
-import {
-  updateDocumentRoomUsers,
-  emitJoinDocumentRoom,
-  emitLeaveDocumentRoom
-} from '../../sockets/SocketProvider'
+import { updateDocumentRoomUsers, emitJoinDocumentRoom, emitLeaveDocumentRoom } from '../../sockets/SocketProvider'
+
 import DocumentUsersComponent from './DocumentUsersComponent'
 
-const DocumentUsersContainer = () => {
+const DocumentUsersContainer = ({ fetchUsersWithIds, usersById, usersLoadedById }) => {
   const { documentId } = useParams()
   const { state: socketState, dispatch: socketDispatch, documentSocket } = useDocumentSocket()
 
   const activeUsers = socketState.activeDocumentUsers[documentId]
   const currentDocumentRoom = socketState.currentDocumentRoom
+  const unloadedIds = activeUsers && activeUsers.filter((userId) => {
+    return usersLoadedById[userId] === undefined
+  })
+
+  useEffect(() => {
+    if (unloadedIds && unloadedIds.length > 0) {
+      fetchUsersWithIds(unloadedIds)
+    }
+  }, [fetchUsersWithIds, unloadedIds])
 
   useEffect(() => {
     socketDispatch(emitJoinDocumentRoom(socketDispatch, documentId))
@@ -33,6 +42,18 @@ const DocumentUsersContainer = () => {
 }
 
 DocumentUsersContainer.propTypes = {
+  fetchUsersWithIds: PropTypes.func.isRequired,
+  usersById: PropTypes.object,
+  usersLoadedById: PropTypes.object,
 }
 
-export default DocumentUsersContainer
+const mapStateToProps = (state) => {
+  return {
+    usersById: state.users.usersById,
+    usersLoadedById: state.users.loading.byId,
+  }
+}
+
+const mapDispatchToProps = { fetchUsersWithIds }
+
+export default connect(mapStateToProps, mapDispatchToProps)(DocumentUsersContainer)
