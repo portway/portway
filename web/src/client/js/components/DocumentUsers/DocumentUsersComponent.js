e2bb89d33142dd7e775459e47b62cdb0ae774897
Jay Contonio <jay@conton.io>
2020-03-04 10:26:15 -0800

    Styling of faces

--- web/src/client/js/components/DocumentUsers/DocumentUsersComponent.js
+++ web/src/client/js/components/DocumentUsers/DocumentUsersComponent.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './_DocumentUsers.scss'
+
 const DocumentUsersComponent = ({ activeUsers }) => {
   return (
     <div className="document-users">
-      {activeUsers && activeUsers.map((activeUser) => {
-        return <img key={activeUser.id} src={activeUser.avatar} width="48" height="48" alt={activeUser.name} />
-      })}
+      <ol className="document-users__list">
+        {activeUsers && activeUsers.map((activeUser) => {
+          if (activeUser) {
+            if (activeUser.avatar) {
+              return (
+                <li className="document-users__list-item" key={activeUser.id}>
+                  <img
+                    alt={activeUser.name}
+                    aria-label={`Image of ${activeUser.name}`}
+                    className="document-users__avatar"
+                    height="32"
+                    src={activeUser.avatar}
+                    title={`${activeUser.name} is viewing this document`}
+                    width="32"
+                  />
+                </li>
+              )
+            }
+            return (
+              <li className="document-users__list-item" key={activeUser.id}>
+                <div
+                  aria-label={`${activeUser.name}’s initial`}
+                  className="document-users__placeholder"
+                  title={`${activeUser.name} is viewing this document`}
+                >
+                  {activeUser.name.charAt(0)}
+                </div>
+              </li>
+            )
+          }
+        })}
+      </ol>
     </div>
   )
 }
