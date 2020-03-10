117dea7df4736bf9f4eab4022a5a5c925d0a71f5
JJ Idt <johnidt@gmail.com>
2020-03-10 12:55:27 -0700

    fix string and number conflict updates

--- web/src/client/js/components/FieldNumber/FieldNumberComponent.js
+++ web/src/client/js/components/FieldNumber/FieldNumberComponent.js
@@ -1,35 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 
 const FieldNumberComponent = ({ field, onBlur, onChange, onFocus, readOnly }) => {
   const [fieldValue, setFieldValue] = useState(field.value)
 
+  const isEditingRef = useRef(false)
+
   useEffect(() => {
-    setFieldValue(field.value)
+    if (!isEditingRef.current) {
+      setFieldValue(field.value)
+    }
   }, [field.value])
+
+  const handleChange = (e) => {
+    // API will only handle 15 significant digits for number fields
+    if (e.target.value.length > 15) return
+    const num = Number.parseFloat(e.target.value)
+    if (!isNaN(num)) {
+      setFieldValue(num)
+      onChange(field.id, num)
+    }
+    if (e.target.value === '') {
+      setFieldValue('')
+      onChange(field.id, null)
+    }
+  }
 
   return (
     <input
       className="document-field__number"
       value={fieldValue == null ? '' : fieldValue}
-      onBlur={(e) => { onBlur(field.id, field.type, field) }}
-      onChange={(e) => {
-        // API will only handle 15 significant digits for number fields
-        if (e.target.value.length > 15) return
-        const num = Number.parseFloat(e.target.value)
-        if (!isNaN(num)) {
-          setFieldValue(num)
-          onChange(field.id, num)
-        }
-        if (e.target.value === '') {
-          setFieldValue('')
-          onChange(field.id, null)
-        }
-      }}
+      onBlur={(e) => { onBlur(field.id, field.type, field); isEditingRef.current = false; handleChange(e) }}
+      onChange={handleChange}
       onFocus={(e) => {
         if (!readOnly) {
           onFocus(field.id, field.type, field)
           e.target.select()
+          isEditingRef.current = true
         }
       }}
       placeholder="A number value..."
