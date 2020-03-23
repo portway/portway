739fb2a1b1635b2185a3a66f8bda7e5df5a6d72e
Dirk Heniges <dheniges@gmail.com>
2020-03-23 16:54:52 -0700

    increase ability for exponential backoff on socketio connection

--- web/src/client/js/sockets/SocketProvider.js
+++ web/src/client/js/sockets/SocketProvider.js
@@ -8,7 +8,13 @@ const token = getCookieValue('token')
 // eslint-disable-next-line no-undef
 const documentUrl = new URL(`/documents?token=${token}`, SYNC_URL)
 
-const documentSocket = openSocket(documentUrl.href)
+// socket-io will exponentially backoff connection attempts with
+// a randomization factor and the reconnectionDelay, up to reconnectionDelayMax
+const documentSocket = openSocket(documentUrl.href, {
+  reconnectionAttempts: 100,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 1200000 // 20 minutes
+})
 
 documentSocket.on('connect', () => {
   console.info('Connected to sync service')
