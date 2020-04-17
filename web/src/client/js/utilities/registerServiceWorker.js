14230d4ea3f6404f83f89542e9e082efe93b6329
Jay Contonio <jay@conton.io>
2020-04-17 13:27:46 -0700

    Updating copy

--- web/src/client/js/utilities/registerServiceWorker.js
+++ web/src/client/js/utilities/registerServiceWorker.js
@@ -20,7 +20,7 @@ export default function register() {
                   console.info('An update to Portway is available. Refresh for the latest updates!')
                   const message = (
                     <>
-                      🚚 An update to Portway has been delivered. <a href={location.href}>Refresh for the latest updates!</a>
+                      🚚 An update to Portway has been delivered. <a href={location.href}>Refresh for the latest changes!</a>
                     </>
                   )
                   Store.dispatch(createNotification(message, NOTIFICATION_TYPES.SUCCESS))
