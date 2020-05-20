dfdb2b4d43b4ae37ba431201387444c91de6d1d2
Dirk Heniges <dheniges@gmail.com>
2020-05-19 17:13:17 -0700

    new admin route for updating org billing status and fetching orgs

--- api/src/controllers/admin.js
+++ api/src/controllers/admin.js
@@ -7,29 +7,33 @@ import adminAuth from '../libs/auth/adminAuth'
 import demoSignupCoordinator from '../coordinators/demoSignup'
 import BusinessOrganization from '../businesstime/organization'
 import organizationCoordinator from '../coordinators/organization'
-
-/**
- * Sample Payload
- *
-{
-  "adminKey": "****",
-  "name": "Org Name",
-  "users": [
-    {
-      "name": "BobbyBong 1",
-      "email": "example1@bonkeybong.com",
-      "role": 1
-    }
-  ]
-}
-*/
+import billingCoordinator from '../coordinators/billing'
 
 const adminController = function(router) {
+  /**
+   * Sample Payload
+   *
+  {
+    "adminKey": "****",
+    "name": "Org Name",
+    "users": [
+      {
+        "name": "BobbyBong 1",
+        "email": "example1@bonkeybong.com",
+        "role": 1
+      }
+    ]
+  }
+  */
   router.post('/demoAccount',
     bodyParser.json(),
     adminAuth,
     createAccount
   )
+
+  router.get('/organizations', adminAuth, getOrganizations)
+
+  router.post('/organizations/:id/updateBillingStatus', adminAuth, updateOrgBillingStatus)
 
   router.delete('/organizations/:id',
     adminAuth,
@@ -49,6 +53,25 @@ const createAccount = async function(req, res, next) {
     await demoSignupCoordinator.createUsersAndOrganization(name, users)
     res.json({ success: true })
   } catch (e) {
+    next(e)
+  }
+}
+
+const getOrganizations = async function(req, res, next) {
+  try {
+    // TODO: switch back to findAll() after testing
+    const orgs = await BusinessOrganization.findAll()
+    res.json({data: orgs})
+  } catch (e) {
+    next(e)
+  }
+}
+
+const updateOrgBillingStatus = async function(req, res, next) {
+  try {
+    await billingCoordinator.fetchCustomerAndSetSubscriptionDataOnOrg(req.params.id)
+    res.status(200).send()
+  } catch(e) {
     next(e)
   }
 }
