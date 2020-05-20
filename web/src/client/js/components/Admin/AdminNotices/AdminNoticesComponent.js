34caf745872eed30386fbf6dd39882e1f2d7c073
Jay Contonio <jay@conton.io>
2020-05-20 11:14:33 -0700

    New notice styles using new Flash component

--- web/src/client/js/components/Admin/AdminNotices/AdminNoticesComponent.js
+++ web/src/client/js/components/Admin/AdminNotices/AdminNoticesComponent.js
@@ -8,15 +8,14 @@ import {
   PLAN_TITLES,
   PLAN_TYPES,
   SUPPORT_EMAIL,
-  TRIAL_ENDED,
   TRIALING_STATUSES
 } from 'Shared/constants'
 
-import { InfoIcon } from 'Components/Icons'
+import FlashComponent from 'Components/Flash/FlashComponent'
+
 import './_AdminNotices.scss'
 
 const AdminNoticesComponent = ({ organization, subscription }) => {
-  const colorDanger = getComputedStyle(document.documentElement).getPropertyValue('--color-red-dark')
   const lockedAccountStatusesMinusInactive = LOCKED_ACCOUNT_STATUSES.filter((status) => {
     return status !== ORG_SUBSCRIPTION_STATUS.INACTIVE
   })
@@ -26,9 +25,8 @@ const AdminNoticesComponent = ({ organization, subscription }) => {
   return (
     <div className="admin-notices">
       {TRIALING_STATUSES.includes(organization.subscriptionStatus) &&
-      <div className="admin-notices__notice">
+      <FlashComponent className="admin-notices__notice">
         <div className="admin-notices__notice-title">
-          <InfoIcon width="22" height="22" />
           <div className="admin-notices__notice-title-content">
             <h2>Your trial ends {trialEnds && <>{trialEndsInDays}, on {moment(trialEnds).format('MMMM Do')}</>}</h2>
             <p className="note">
@@ -39,40 +37,53 @@ const AdminNoticesComponent = ({ organization, subscription }) => {
         <p>
           <a href="#payment">Add your payment information below</a> to activate your account or to upgrade to a {PLAN_TITLES[PLAN_TYPES.MULTI_USER]}.
         </p>
-      </div>
+      </FlashComponent>
       }
-      {organization.subscriptionStatus === TRIAL_ENDED &&
-      <div className="admin-notices__notice">
+      {organization.subscriptionStatus === ORG_SUBSCRIPTION_STATUS.TRIAL_ENDED &&
+      <FlashComponent className="admin-notices__notice" type="warning">
         <div className="admin-notices__notice-title">
-          <InfoIcon width="22" height="22" />
           <div className="admin-notices__notice-title-content">
             <h2>Your trial has ended</h2>
             <p className="note">
-              Thanks for checking out Portway! We’re sad to see you go. Your account will be completely erased soon. If you change your mind you can <a href="#payment">activate your account now</a>.
+              Thanks for checking out Portway! We’re sad to see you go.
+              Your account will be completely erased soon.
+              <br />If you change your mind you can <a href="#payment">activate your account now</a>.
             </p>
           </div>
         </div>
         <p>
           <a href="#payment">Add your payment information below</a> to activate your account.
         </p>
-      </div>
+      </FlashComponent>
       }
       {organization.subscriptionStatus === ORG_SUBSCRIPTION_STATUS.INACTIVE &&
-      <div className="admin-notices__notice admin-notices__notice--danger">
-        <h2 className="admin-notices__notice-title danger"><InfoIcon width="22" height="22" fill={colorDanger} /> Inactive</h2>
+      <FlashComponent className="admin-notices__notice" type="error">
+        <div className="admin-notices__notice-title">
+          <div className="admin-notices__notice-title-content">
+            <h2 className="danger">Inactive</h2>
+          </div>
+        </div>
         <p>Thanks for checking out Portway! We are in the process of removing your account.</p>
-      </div>
+      </FlashComponent>
       }
       {lockedAccountStatusesMinusInactive.includes(organization.subscriptionStatus) &&
-      <div className="admin-notices__notice admin-notices__notice--danger">
-        <h2 className="admin-notices__notice-title danger"><InfoIcon width="22" height="22" fill={colorDanger} /> Past due</h2>
-        <p>We cannot successfully bill you with your current payment information.</p>
-        <p>Please update your payment information below to activate your account.</p>
-      </div>
+      <FlashComponent className="admin-notices__notice" type="error">
+        <div className="admin-notices__notice-title">
+          <div className="admin-notices__notice-title-content">
+            <h2 className="danger">Past due</h2>
+            <p className="note">We cannot successfully bill you with your current payment information.</p>
+          </div>
+        </div>
+        <p>Please <a href="#payment">update your payment information below</a> to activate your account.</p>
+      </FlashComponent>
       }
       {organization.subscriptionStatus === ORG_SUBSCRIPTION_STATUS.PENDING_CANCEL && subscription &&
-      <div className="admin-notices__notice">
-        <h2 className="admin-notices__notice-title danger"><InfoIcon width="22" height="22" fill={colorDanger} />Account canceled</h2>
+      <FlashComponent className="admin-notices__notice" type="error">
+        <div className="admin-notices__notice-title">
+          <div className="admin-notices__notice-title-content">
+            <h2 className="danger">Account canceled</h2>
+          </div>
+        </div>
         <p>
           Your current period ends <b>{moment.unix(subscription.cancelAt || subscription.currentPeriodEnd).format('MMMM Do, YYYY')}</b>.
           You have access to your projects and documents until then.
@@ -80,7 +91,7 @@ const AdminNoticesComponent = ({ organization, subscription }) => {
         <p>
           If you need assistance, please email <a href={`mailto:${SUPPORT_EMAIL}`}>{SUPPORT_EMAIL}</a>.
         </p>
-      </div>
+      </FlashComponent>
       }
     </div>
   )
