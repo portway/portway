86071702414750f9ea5a9ca22e99d1af8b00ad72
Dirk Heniges <dheniges@gmail.com>
2020-06-03 11:10:31 -0700

    a little dig action

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,19 @@
 import app from './app'
 import { connect, loadModels } from './db/dbConnector'
+const { exec } = require('child_process')
 
 const port = process.env.API_PORT
+
+exec('dig +trace us.data.logs.insight.rapid7.com', (err, stdout, stderr) => {
+  if (err) {
+    // node couldn't execute the command
+    return;
+  }
+
+  // the *entire* stdout and stderr (buffered)
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
 
 
 //CONNECT TO THE DB
