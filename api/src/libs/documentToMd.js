e1870280aa9bebbe642fef0dc2bca06af042584e
JJ Idt <johnidt@gmail.com>
2020-07-01 13:13:51 -0700

    parse image and date fields

--- api/src/libs/documentToMd.js
+++ api/src/libs/documentToMd.js
@@ -1,28 +1,45 @@
 import { FIELD_TYPES, FIELD_TYPE_MODELS } from "../constants/fieldTypes"
+import url from "url"
 
 const documentToMd = function(document) {
   const frontMatter = getDocumentFrontmatter(document)
-  return frontMatter
+  const content = getDocumentContent(document)
+  return frontMatter + content
 }
 
 const getDocumentFrontmatter = function(document) {
   const fieldFrontmatter = document.fields.reduce((cur, field) => {
     if (field.type === FIELD_TYPES.TEXT) return cur
-    return cur + `${field.name}: ${field.value}\n`
+    return cur + `${field.name}: ${getFieldValueByType(field, document.id)}\n`
   }, '')
 
-  const content = document.fields.reduce((cur, field) => {
+  return `---\ntitle: ${document.name}` +
+  `\n${fieldFrontmatter}` +
+  `---\n`
+}
+
+const getDocumentContent = function(document) {
+  return document.fields.reduce((cur, field) => {
     if (field.type === FIELD_TYPES.TEXT) {
       const fieldValue = field.value ? field.value : ''
       return cur + `${fieldValue}\n\n`
     }
     return cur
   }, '')
+}
 
-  return `---\ntitle: ${document.name}` +
-  `\n${fieldFrontmatter}` +
-  `---\n` +
-  content
+const getFieldValueByType = function(field, documentId) {
+  switch (field.type) {
+    case FIELD_TYPES.IMAGE:
+    case FIELD_TYPES.FILE:
+      const parsedUrl = url.parse(field.value)
+      const filename = parsedUrl.path.split('/')[4]
+      return `./${documentId}/${filename}`
+    case FIELD_TYPES.DATE:
+      return field.value.toISOString()
+    default:
+      return field.value
+  }
 }
 
 export default documentToMd
