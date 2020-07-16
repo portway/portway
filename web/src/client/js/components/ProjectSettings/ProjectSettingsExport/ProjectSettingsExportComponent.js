8a6161d0602d67683fee43373bdf440fab0fb6a8
Jay Contonio <jay@conton.io>
2020-07-16 09:16:19 -0700

    Adding check for zero documents

--- web/src/client/js/components/ProjectSettings/ProjectSettingsExport/ProjectSettingsExportComponent.js
+++ web/src/client/js/components/ProjectSettings/ProjectSettingsExport/ProjectSettingsExportComponent.js
@@ -7,17 +7,26 @@ import { ArrowIcon } from 'Components/Icons'
 
 import Spinner from 'Components/Spinner/SpinnerComponent'
 
+function documentLanguage(numberOfDocuments) {
+  if (numberOfDocuments === 0) {
+    return <p className="small">No documents to export</p>
+  }
+  return <p className="small">This export includes <b>{numberOfDocuments} documents</b></p>
+}
+
 const ProjectSettingsExportComponent = ({ loading, numberOfDocuments, project }) => {
   return (
     <section>
-      <Link to={`${PATH_PROJECT}/${project.id}`} className="link link--back"><ArrowIcon direction="left" /> Back to project</Link>
+      <Link to={`${PATH_PROJECT}/${project.id}`} className="link link--back">
+        <ArrowIcon direction="left" /> Back to project
+      </Link>
       <h2>Export your project</h2>
       <p>Back up or move your data out of Portway with the click of a button.</p>
-      <button className="btn">
+      <button className="btn" disabled={numberOfDocuments === 0}>
         {loading && <Spinner />}
         {!loading && <>Download {project.name}.zip</>}
       </button>
-      <p className="small">This export includes <b>{numberOfDocuments} documents</b></p>
+      {documentLanguage(numberOfDocuments)}
     </section>
   )
 }
