2b4b4a7a9d542dc8d9f98834b43ab918b1e8b3a7
Jay Contonio <jay@conton.io>
2020-10-26 11:27:20 -0700

    Making minor updates

--- web/src/client/js/components/ProjectSettings/ProjectSettingsWebhooks/ProjectSettingsDeliveries.js
+++ web/src/client/js/components/ProjectSettings/ProjectSettingsWebhooks/ProjectSettingsDeliveries.js
@@ -6,19 +6,29 @@ import cx from 'classnames'
 const ProjectSettingsDeliveries = ({ deliveries }) => {
   return (
     <div className="webhooks-list__delivery-data">
-      {deliveries.map((delivery) => {
-        const codeClasses = cx({
-          'webhooks-list__delivery-code': true,
-          'webhooks-list__delivery-code--success': delivery.resultCode < 300,
-          'webhooks-list__delivery-code--error': delivery.resultCode > 300,
-        })
-        return (
-          <div key={delivery.id}>
-            <span className={codeClasses}>{delivery.resultCode}</span>
-            <span className="webhooks-list__delivery-date">{moment(delivery.createdAt).format('YYYY-MM-DD')}</span>
-          </div>
-        )
-      })}
+      <table>
+        <thead>
+          <tr>
+            <th className="webhooks-list__header">Response code</th>
+            <th className="webhooks-list__header">Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {deliveries.map((delivery) => {
+            const codeClasses = cx({
+              'webhooks-list__delivery-code': true,
+              'webhooks-list__delivery-code--success': delivery.resultCode < 300,
+              'webhooks-list__delivery-code--error': delivery.resultCode > 300,
+            })
+            return (
+              <tr key={delivery.id}>
+                <td className={codeClasses}>{delivery.resultCode}</td>
+                <td className="webhooks-list__delivery-date">{moment(delivery.createdAt).format('YYYY-MM-DD')}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
