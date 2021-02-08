cf7a2f90ca27f260f60e295186aad36dbe58640a
JJ Idt <johnidt@gmail.com>
2021-02-08 13:13:52 -0800

    try wildcard

--- sync/src/libs/io.js
+++ sync/src/libs/io.js
@@ -1,15 +1,11 @@
 import io from 'socket.io'
 
-const { CLIENT_URL } = process.env
-
 let loadedIO
 
 export function loadIO(server) {
   loadedIO = io(server, {
     cors: {
-      origin: CLIENT_URL,
-      methods: ['GET', 'POST'],
-      credentials: true
+      origin: '*'
     }
   })
   return loadedIO
