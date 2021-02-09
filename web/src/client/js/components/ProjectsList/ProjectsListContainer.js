a495928778bb1d2b20216a222bb41248131eb9f0
JJ Idt <johnidt@gmail.com>
2021-02-09 10:27:38 -0800

    remove unused useEffect

--- web/src/client/js/components/ProjectsList/ProjectsListContainer.js
+++ web/src/client/js/components/ProjectsList/ProjectsListContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { useParams } from 'react-router-dom'
